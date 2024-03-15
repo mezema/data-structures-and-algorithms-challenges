5cc9712377a217fed7d8507b6b663425667db59f
Mark Ezema <43732681+mezema@users.noreply.github.com>
2024-03-15 14:00:24 -0400

    python questions

--- questions/00002-maximum-sum-subarray-of-size-k/template.ts
+++
@@ -1,4 +0,0 @@
-const maxSubArrayOfSizeK = (k: number, arr: number[]): number => {
-    // TODO: Write your code here
-    return -1;
-};
--- questions/00003-smallest-subarray-with-a-greater-sum/template.ts
+++
@@ -1,4 +0,0 @@
-const smallest_subarray_with_given_sum = (arr: number[], s: number): number => {
-    // TODO: Write your code here
-    return -1;
-};
