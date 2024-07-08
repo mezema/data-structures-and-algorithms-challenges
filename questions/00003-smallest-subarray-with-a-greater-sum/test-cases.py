808a26e67e3840f10d9b918e4bcf306073b6bfe6
Mark Ezema <43732681+mezema@users.noreply.github.com>
2024-07-08 18:59:53 -0400

    Update test-cases.py

--- questions/00003-smallest-subarray-with-a-greater-sum/test-cases.py
+++ questions/00003-smallest-subarray-with-a-greater-sum/test-cases.py
@@ -1,15 +1,10 @@
+import unittest
+
 class TestSmallestSubarraySum(unittest.TestCase):
     def test_examples(self):
         self.assertEqual(smallest_subarray_sum(7, [2, 1, 5, 2, 3, 2]), 2)
         self.assertEqual(smallest_subarray_sum(7, [2, 1, 5, 2, 8]), 1)
         self.assertEqual(smallest_subarray_sum(8, [3, 4, 1, 1, 6]), 3)
-
-    def test_additional_cases(self):
-        self.assertEqual(smallest_subarray_sum(100, [1, 2, 3, 4, 5]), 0)
-        self.assertEqual(smallest_subarray_sum(4, [1, 2, 3, 4, 5]), 1)
-        self.assertEqual(smallest_subarray_sum(15, [1, 2, 3, 4, 5]), 5)
-        self.assertEqual(smallest_subarray_sum(10, [1, 2, 3, 4, 5, 6]), 2)
-        self.assertEqual(smallest_subarray_sum(9, [1, 2, 8, 4, 3]), 1)
 
 # Manually create and run the test suite
 def run_tests():
